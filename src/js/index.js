5d8114a6b89a531aaf812ed0e9a8a2d860794fe0
Hansana Dasanayaka <75363593+DevHanza@users.noreply.github.com>
2024-09-20 05:09:53 +0530

    Navbar Done. ✅

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
-function showMenu() {
-  var navItems = document.getElementById("navItems");
-  navItems.classList.toggle("hidden");
-  navItems.classList.toggle("navbar-toggle");
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("height-auto");
+  menu.classList.toggle("opacity-0");
+
+  var menuList = document.querySelectorAll("#menu li");
+  for (let li of menuList) {
+    li.classList.toggle("hidden");
+  }
+
+  var navWrapper = document.getElementById("nav-wrapper");
+  navWrapper.classList.toggle("drop-shadow");
 }
